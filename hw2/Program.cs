792d78ff82be26dba8cd8a176bcbb0a89a497bd0
Artem <ermilintemka@gmail.com>
2022-07-06 22:27:13 +0300

    HW2 and HW3

---
+++ hw2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int GetNumber()
+{
+    int result = 0;
+    Console.WriteLine("Введите число N: ");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int numberN = (GetNumber());
+
+int SumNumber(int numberN)
+{
+
+    int count = Convert.ToString(numberN).Length;
+    int Next = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        Next = numberN - numberN % 10;
+        result = result + (numberN - Next);
+        numberN = numberN / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
---
+++ hw3/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+ Напишите программу, которая задаёт массив 
+ из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+/*void PrintArrayForEach(int[] array)
+{
+    foreach (int item in array)
+    {
+        //int item = array[i], в цикле по i 
+
+        Console.Write($"{item} ");
+    }
+}*/
+/*//Метод вывода рандомного массива из 8 элементов
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+var array = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 10);
+}
+
+Console.WriteLine("Рандомный массив из 8 элементов: ");
+PrintArray(array);
+Console.WriteLine();
+
+
+/*Console.WriteLine("Вывод через цикл foreach: ");
+PrintArrayForEach(array);*/
+
+//Console.WriteLine($"{array[2]}");
+
+//Console.WriteLine(iarray);
+
+//Метод вывода рандомного массива из N элементов
+int GetNumber()
+{
+    int result = 0;
+    Console.WriteLine("Введите кол-во элементов массива:");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+void PrArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int N = GetNumber();
+var array = new int[N];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 10);
+}
+
+Console.WriteLine($"Рандомный массив из {N} элементов: ");
+PrArray(array);
+Console.WriteLine();
