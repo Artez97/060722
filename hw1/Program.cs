f834ccd21cf809d0231e2a8fe5615fe5932adcfd
Artem <ermilintemka@gmail.com>
2022-07-06 22:25:55 +0300

    add hw1

---
+++ hw1/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает 
+на вход два числа (A и B) и возводит число A в 
+натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int GetNumberA()
+{
+    int result = 0;
+    Console.WriteLine("Введите число A:");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int GetNumberB()
+{
+    int result = 0;
+    Console.WriteLine("Введите число B:");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+double GetA = GetNumberA();
+double GetB = GetNumberB();
+double GetStep = Math.Pow(GetA, GetB);
+
+Console.WriteLine($"{GetA}^{GetB}={GetStep}");
+
